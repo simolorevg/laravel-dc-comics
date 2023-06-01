2f6ea2858418be6853b789513b1c49d8c9adfb0d
simolorevg <simoneviaggi@libero.it>
2023-06-01 15:58:28 +0200

    fixed some style on the show page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,21 +1,25 @@
 @extends('layouts.app')
 @section('content')
-    <a class="btn btn-primary" href="{{ route('comics.index') }}">Indietro</a>
-    <div class="container">
+    <div class="show-container">
         <h2>{{ $comic->title }}</h2>
-        <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+        <div class="show-img-container">
+            <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+        </div>
         <p>{{ $comic->series }}</p>
         <p>{{ $comic->Type }}</p>
         <p>In vendita dal: {{ $comic->sale_date }}. Prezzo: {{ $comic->price }}</p>
         <p>{{ $comic->description }}</p>
-        <a class="btn btn-warning" href="{{ route('comics.edit', $comic->id) }}">
-            Modifica elemento
-        </a>
-        <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button class="btn btn-danger" type="submit" onclick="return confirm('Do you want delete this item?')"
-                value="Delete"> Elimina</button>
-        </form>
+        <div class="btn-show-container">
+            <a class="btn btn-warning" href="{{ route('comics.edit', $comic->id) }}">
+                Modifica elemento
+            </a>
+            <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger" type="submit" onclick="return confirm('Do you want delete this item?')"
+                    value="Delete"> Elimina</button>
+            </form>
+            <a class="btn btn-primary" href="{{ route('comics.index') }}">Indietro</a>
+        </div>
     </div>
 @endsection
